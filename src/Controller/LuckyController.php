c2cf47f345f3495d3e8a2e526efb4b0604e77347
Peter Ton <peter.ton@coolblue.nl>
2023-08-06 18:13:34 +0200

    Add lucky number example page

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
