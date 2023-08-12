1975281468e504f7f8b9e49a5d3daee882773c4a
Peter Ton <peter.ton@coolblue.nl>
2023-08-12 23:20:56 +0200

    Use deployer for deployments

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+set('repository', 'git@github.com:cbpeter/legion-td2-app.git');
+
+add('shared_files', ['.env.local']);
+add('shared_dirs', ['data']);
+add('writable_dirs', ['data']);
+
+// Hosts
+host('31.222.229.195')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/legion-td2-app');
+
+// Tasks
+task('deploy:build_assets', function () {
+    cd('{{release_path}}');
+    run('bin/console asset-map:compile');
+});
+task('deploy:import_data', function () {
+    cd('{{release_path}}');
+    run('bin/console app:download');
+});
+
+// Hooks
+before('deploy:unlock', 'deploy:build_assets');
+before('deploy:success', 'deploy:import_data');
+after('deploy:failed', 'deploy:unlock');
