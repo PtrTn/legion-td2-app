ececb98c535505ad31cf9160d95c4e51b6e88dbe
Peter Ton <peter.ton@coolblue.nl>
2023-08-13 16:35:06 +0200

    Optimize production environment

--- deploy.php
+++ deploy.php
@@ -24,6 +24,10 @@ task('deploy:import_data', function () {
     cd('{{release_path}}');
     run('bin/console app:download');
 });
+task('deploy:optimize', function () {
+    cd('{{release_path}}');
+    run('composer dump-env prod');
+});
 task('deploy:remove_files', function () {
     cd('{{release_path}}');
     run('rm -rf .github');
@@ -34,6 +38,7 @@ task('deploy:remove_files', function () {
 
 // Hooks
 after('deploy:symlink', 'deploy:build_assets');
+after('deploy:symlink', 'deploy:optimize');
 before('deploy:success', 'deploy:import_data');
 before('deploy:success', 'deploy:remove_files');
 after('deploy:failed', 'deploy:unlock');
--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,14 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use function dirname;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+    }
 }
